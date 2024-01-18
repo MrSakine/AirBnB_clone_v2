2fd7380d7502f110eb286d02afcfb380c9465411
hamzahdili2001 <hdilihamza955@gmail.com>
2024-01-18 16:06:42 +0100

    [fix] add user default values

--- models/user.py
+++ models/user.py
@@ -5,14 +5,26 @@ from sqlalchemy import Column, String
 from models.place import Place
 from models.review import Review
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship("Place", cascade="all, delete", backref="user")
-    reviews = relationship("Review", cascade="all, delete", backref="user")
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship(
+            "Place", cascade="all, delete", backref="user"
+        )
+        reviews = relationship(
+            "Review", cascade="all, delete", backref="user"
+        )
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
